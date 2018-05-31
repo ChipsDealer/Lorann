263b5f5d514bd6515421d4150f789f960a6e6b6f
Thibaut <thibaut.lemmel@viacesi.fr>
2018-05-31 14:34:47 +0200

    Javadoc for Collision

--- controller/src/main/java/controller/Collision.java
+++ controller/src/main/java/controller/Collision.java
@@ -1,184 +1,421 @@
 package controller;
 
-public class Collision {
+/**
+* <h1>The Class Collision manage the collision beetween the elements.</h1>
+*/
+public abstract class Collision {
 
-  public Collision(Lorann LORANN, Treasure TREASURE){
-
-  }
-
-  public Collision(Lorann LORANN, Os OS){
-
-  }
-
-  public Collision(Lorann LORANN, EnergicBubble ENERGICBUBBLE){
-
-  }
-
-  public Collision(Lorann LORANN, BadDoor BADDOOR){
+  /**
+  * Check if the source element is moving on another element
+  * @param source element who want to move
+  * @param target location where the source element want to go
+  */
+  public boolean checkCollision(Elements source, Elements target){
 
   }
 
-  public Collision(Lorann LORANN, KindDoor KINDDOOR){
+  /**
+  * Call the method for this collision and answer if the source element can move
+  * @param LORANN player who want to move
+  * @param TREASURE the treasure where the other element want to move
+  */
+  public boolean actionCollision(Lorann LORANN, Treasure TREASURE){
 
   }
 
-  public Collision(Lorann LORANN, Kyracj KYRACJ){
+  /**
+  * Call the method for this collision and answer if the source element can move
+  * @param LORANN player who want to move
+  * @param BONE the bone where the other element want to move
+  */
+  public boolean actionCollision(Lorann LORANN, Bone BONE){
 
   }
 
-  public Collision(Lorann LORANN, Cargyv CARGYV){
+  /**
+  * Call the method for this collision and answer if the source element can move
+  * @param LORANN player who want to move
+  * @param ENERGICBUBBLE the energic bubble where the other element want to move
+  */
+  public boolean actionCollision(Lorann LORANN, EnergicBubble ENERGICBUBBLE){
 
   }
 
-  public Collision(Lorann LORANN, Arrbarr ARRBARR){
+  /**
+  * Call the method for this collision and answer if the source element can move
+  * @param LORANN player who want to move
+  * @param BADDOOR the bad door where the other element want to move
+  */
+  public boolean actionCollision(Lorann LORANN, BadDoor BADDOOR){
 
   }
 
-  public Collision(Lorann LORANN, Maarcg MAARCG){
+  /**
+  * Call the method for this collision and answer if the source element can move
+  * @param LORANN player who want to move
+  * @param KINDDOOR the kind door where the other element want to move
+  */
+  public boolean actionCollision(Lorann LORANN, KindDoor KINDDOOR){
 
   }
 
-  public Collision(Lorann LORANN, Sortilege SORTILEGE){
+  /**
+  * Call the method for this collision and answer if the source element can move
+  * @param LORANN player who want to move
+  * @param KYRACJ the first demon where the other element want to move
+  */
+  public boolean actionCollision(Lorann LORANN, Kyracj KYRACJ){
 
   }
 
-  public Collision(Kyracj KYRACJ, Treasure TREASURE){
+  /**
+  * Call the method for this collision and answer if the source element can move
+  * @param LORANN player who want to move
+  * @param CARGYV the second demon where the other element want to move
+  */
+  public boolean actionCollision(Lorann LORANN, Cargyv CARGYV){
 
   }
 
-  public Collision(Kyracj KYRACJ, Os OS){
+  /**
+  * Call the method for this collision and answer if the source element can move
+  * @param LORANN player who want to move
+  * @param ARRBARR the third demon where the other element want to move
+  */
+  public boolean actionCollision(Lorann LORANN, Arrbarr ARRBARR){
 
   }
 
-  public Collision(Kyracj KYRACJ, EnergicBubble ENERGICBUBBLE){
+  /**
+  * Call the method for this collision and answer if the source element can move
+  * @param LORANN player who want to move
+  * @param MAARCG the fourth demon where the other element want to move
+  */
+  public boolean actionCollision(Lorann LORANN, Maarcg MAARCG){
 
   }
 
-  public Collision(Kyracj KYRACJ, BadDoor BADDOOR){
+  /**
+  * Call the method for this collision and answer if the source element can move
+  * @param LORANN player who want to move
+  * @param SPELL the spell where the other element want to move
+  */
+  public boolean actionCollision(Lorann LORANN, Spell SPELL){
 
   }
 
-  public Collision(Kyracj KYRACJ, KindDoor KINDDOOR){
+  /**
+  * Call the method for this collision and answer if the source element can move
+  * @param KYRACJ the first demon who want to move
+  * @param TREASURE the treasure where the other element want to move
+  */
+  public boolean actionCollision(Kyracj KYRACJ, Treasure TREASURE){
 
   }
 
-  public Collision(Kyracj KYRACJ, Cargyv CARGYV){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param KYRACJ the first demon who want to move
+    * @param BONE the bone where the other element want to move
+    */
+  public boolean actionCollision(Kyracj KYRACJ, Bone BONE){
 
   }
 
-  public Collision(Kyracj KYRACJ, Arrbarr ARRBARR){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param KYRACJ the first demon who want to move
+    * @param ENERGICBUBBLE the energic bubble where the other element want to move
+    */
+  public boolean actionCollision(Kyracj KYRACJ, EnergicBubble ENERGICBUBBLE){
 
   }
 
-  public Collision(Kyracj KYRACJ, Maarcg MAARCG){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param KYRACJ the first demon who want to move
+    * @param BADDOOR the bad door where the player want to move
+    */
+  public boolean actionCollision(Kyracj KYRACJ, BadDoor BADDOOR){
 
   }
 
-  public Collision(Kyracj KYRACJ, Sortilege SORTILEGE){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param KYRACJ the first demon who want to move
+    * @param KINDDOOR the kind door where the other element want to move
+    */
+  public boolean actionCollision(Kyracj KYRACJ, KindDoor KINDDOOR){
 
   }
 
-  public Collision(Cargyv CARGYV, Treasure TREASURE){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param KYRACJ the first demon who want to move
+    * @param CARGYV the second demon where the other element want to move
+    */
+  public boolean actionCollision(Kyracj KYRACJ, Cargyv CARGYV){
 
   }
 
-  public Collision(Cargyv CARGYV, Os OS){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param KYRACJ the first demon who want to move
+    * @param ARRBARR the third demon where the other element want to move
+    */
+  public boolean actionCollision(Kyracj KYRACJ, Arrbarr ARRBARR){
 
   }
 
-  public Collision(Cargyv CARGYV, EnergicBubble ENERGICBUBBLE){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param KYRACJ the first demon who want to move
+    * @param MAARCG the fourth demon where the other element want to move
+    */
+  public boolean actionCollision(Kyracj KYRACJ, Maarcg MAARCG){
 
   }
 
-  public Collision(Cargyv CARGYV, BadDoor BADDOOR){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param KYRACJ the first demon who want to move
+    * @param SPELL the spell where the other element want to move
+    */
+  public boolean actionCollision(Kyracj KYRACJ, Spell SPELL){
 
   }
 
-  public Collision(Cargyv CARGYV, KindDoor KINDDOOR){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param CARGYV the second demon who want to move
+    * @param TREASURE the treasure where the other element want to move
+    */
+  public boolean actionCollision(Cargyv CARGYV, Treasure TREASURE){
 
   }
 
-  public Collision(Cargyv CARGYV, Arrbarr ARRBARR){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param CARGYV the second demon who want to move
+    * @param BONE the bone where the other element want to move
+    */
+  public boolean actionCollision(Cargyv CARGYV, Bone BONE){
 
   }
 
-  public Collision(Cargyv CARGYV, Maarcg MAARCG){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param CARGYV the second demon who want to move
+    * @param ENERGICBUBBLE the energic bubble where the other element want to move
+    */
+  public boolean actionCollision(Cargyv CARGYV, EnergicBubble ENERGICBUBBLE){
 
   }
 
-  public Collision(Cargyv CARGYV, Sortilege SORTILEGE){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param CARGYV the second demon who want to move
+    * @param BADDOOR the bad door where the player want to move
+    */
+  public boolean actionCollision(Cargyv CARGYV, BadDoor BADDOOR){
 
   }
 
-  public Collision(Arrbarr ARRBARR, Treasure TREASURE){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param CARGYV the second demon who want to move
+    * @param KINDDOOR the kind door where the other element want to move
+    */
+  public boolean actionCollision(Cargyv CARGYV, KindDoor KINDDOOR){
 
   }
 
-  public Collision(Arrbarr ARRBARR, Os OS){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param CARGYV the second demon who want to move
+    * @param ARRBARR the third demon where the other element want to move
+    */
+  public boolean actionCollision(Cargyv CARGYV, Arrbarr ARRBARR){
 
   }
 
-  public Collision(Arrbarr ARRBARR, EnergicBubble ENERGICBUBBLE){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param CARGYV the second demon who want to move
+    * @param MAARCG the fourth demon where the other element want to move
+    */
+  public boolean actionCollision(Cargyv CARGYV, Maarcg MAARCG){
 
   }
 
-  public Collision(Arrbarr ARRBARR, BadDoor BADDOOR){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param CARGYV the second demon who want to move
+    * @param SPELL the spell where the other element want to move
+    */
+  public boolean actionCollision(Cargyv CARGYV, Spell SPELL){
 
   }
 
-  public Collision(Arrbarr ARRBARR, KindDoor KINDDOOR){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param ARRBARR the third demon who want to move
+    * @param TREASURE the treasure where the other element want to move
+    */
+  public boolean actionCollision(Arrbarr ARRBARR, Treasure TREASURE){
 
   }
 
-  public Collision(Arrbarr ARRBARR, Maarcg MAARCG){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param ARRBARR the third demon who want to move
+    * @param BONE the bone where the other element want to move
+    */
+  public boolean actionCollision(Arrbarr ARRBARR, Bone BONE){
 
   }
 
-  public Collision(Arrbarr ARRBARR, Sortilege SORTILEGE){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param ARRBARR the third demon who want to move
+    * @param ENERGICBUBBLE the energic bubble where the other element want to move
+    */
+  public boolean actionCollision(Arrbarr ARRBARR, EnergicBubble ENERGICBUBBLE){
 
   }
 
-  public Collision(Maarcg MAARCG, Treasure TREASURE){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param ARRBARR the third demon who want to move
+    * @param BADDOOR the bad door where the player want to move
+    */
+  public boolean actionCollision(Arrbarr ARRBARR, BadDoor BADDOOR){
 
   }
 
-  public Collision(Maarcg MAARCG, Os OS){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param ARRBARR the third demon who want to move
+    * @param KINDDOOR the kind door where the other element want to move
+    */
+  public boolean actionCollision(Arrbarr ARRBARR, KindDoor KINDDOOR){
 
   }
 
-  public Collision(Maarcg MAARCG, EnergicBubble ENERGICBUBBLE){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param ARRBARR the third demon who want to move
+    * @param MAARCG the fourth demon where the other element want to move
+    */
+  public boolean actionCollision(Arrbarr ARRBARR, Maarcg MAARCG){
 
   }
 
-  public Collision(Maarcg MAARCG, BadDoor BADDOOR){
+      /**
+      * Call the method for this collision and answer if the source element can move
+      * @param ARRBARR the third demon who want to move
+      * @param SPELL the spell where the other element want to move
+      */
+  public boolean actionCollision(Arrbarr ARRBARR, Spell SPELL){
 
   }
 
-  public Collision(Maarcg MAARCG, KindDoor KINDDOOR){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param MAARCG the fourth demon who want to move
+    * @param TREASURE the treasure where the other element want to move
+    */
+  public boolean actionCollision(Maarcg MAARCG, Treasure TREASURE){
 
   }
 
-  public Collision(Maarcg MAARCG, Sortilege SORTILEGE){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param MAARCG the fourth demon who want to move
+    * @param BONE the bone where the other element want to move
+    */
+  public boolean actionCollision(Maarcg MAARCG, Bone BONE){
 
   }
 
-  public Collision(Sortilege SORTILEGE, Treasure TREASURE){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param MAARCG the fourth demon who want to move
+    * @param ENERGICBUBBLE the energic bubble where the other element want to move
+    */
+  public boolean actionCollision(Maarcg MAARCG, EnergicBubble ENERGICBUBBLE){
 
   }
 
-  public Collision(Sortilege SORTILEGE, Os OS){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param MAARCG the fourth demon who want to move
+    * @param BADDOOR the bad door where the player want to move
+    */
+  public boolean actionCollision(Maarcg MAARCG, BadDoor BADDOOR){
 
   }
 
-  public Collision(Sortilege SORTILEGE, EnergicBubble ENERGICBUBBLE){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param MAARCG the fourth demon who want to move
+    * @param KINDDOOR the kind door where the other element want to move
+    */
+  public boolean actionCollision(Maarcg MAARCG, KindDoor KINDDOOR){
 
   }
 
-  public Collision(Sortilege SORTILEGE, BadDoor BADDOOR){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param MAARCG the fourth demon who want to move
+    * @param SPELL the spell where the other element want to move
+    */
+  public boolean actionCollision(Maarcg MAARCG, Spell SPELL){
 
   }
 
-  public Collision(Sortilege SORTILEGE, KindDoor KINDDOOR){
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param SPELL the spell who want to move
+    * @param TREASURE the treasure where the other element want to move
+    */
+  public boolean actionCollision(Spell SPELL, Treasure TREASURE){
+
+  }
+
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param SPELL the spell who want to move
+    * @param BONE the bone where the other element want to move
+    */
+  public boolean actionCollision(Spell SPELL, Bone BONE){
+
+  }
+
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param SPELL the spell who want to move
+    * @param ENERGICBUBBLE the energic bubble where the other element want to move
+    */
+  public boolean actionCollision(Spell SPELL, EnergicBubble ENERGICBUBBLE){
+
+  }
+
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param SPELL the spell who want to move
+    * @param BADDOOR the bad door where the player want to move
+    */
+  public boolean actionCollision(Spell SPELL, BadDoor BADDOOR){
+
+  }
+
+    /**
+    * Call the method for this collision and answer if the source element can move
+    * @param SPELL the spell who want to move
+    * @param KINDDOOR the kind door where the other element want to move
+    */
+  public boolean actionCollision(Spell SPELL, KindDoor KINDDOOR){
 
   }
 

677b1543c7e7f0c97f887527555bcdadaf211dff
Thibaut <thibaut.lemmel@viacesi.fr>
2018-05-31 17:21:46 +0200

    Revert "Merge branch 'Léo' into Pierre"
    This reverts commit 52828ab506a4caf50bf2294df29467c916413dfe, reversing
    changes made to 0ea37aaea808b41ffd1fc25ba260100f2e9f501b.

--- model/src/main/java/model/ModelFacade.java
+++ model/src/main/java/model/ModelFacade.java
@@ -1,37 +1,18 @@
 package model;
 
-import java.sql.ResultSet;
-import java.util.ArrayList;
+import java.sql.SQLException;
+import java.util.List;
 
-import model.dao.MapDAO;
-import model.immobile.MotionLessFactory;
+import model.dao.ExampleDAO;
 
 /**
  * <h1>The Class ModelFacade provides a facade of the Model component.</h1>
  *
- * @author Léo Thommes leo.thommes@cesi.fr
+ * @author Jean-Aymeric DIET jadiet@cesi.fr
  * @version 1.0
  */
 public final class ModelFacade implements IModel {
-	
-	
-	/** The Constant MotionLessFactory. */
-	public static MotionLessFactory motionlessfactory;
-	
-	/** The Constant MotionFactory. */
-	public static MotionFactory motionfactory;
-	
-	/** The Constant MapDAO. */
-	public static MapDAO mapDao;
-	
-	
-	/** The ArrayList MotionFactory. */
-	public ArrayList<MotionFactory> mapMotion;
-	
-	/** The ArrayList MotionLessFactory. */
-	public ArrayList<MotionLessFactory> mapMotionLess;
-	
-	
+
     /**
      * Instantiates a new model facade.
      */
@@ -40,10 +21,9 @@ public final class ModelFacade implements IModel {
     }
 
     /*
-    /*
      * (non-Javadoc)
      * @see model.IModel#getExampleById(int)
-     *
+     */
     @Override
     public Example getExampleById(final int id) throws SQLException {
         return ExampleDAO.getExampleById(id);
@@ -52,7 +32,7 @@ public final class ModelFacade implements IModel {
     /*
      * (non-Javadoc)
      * @see model.IModel#getExampleByName(java.lang.String)
-     *
+     */
     @Override
     public Example getExampleByName(final String name) throws SQLException {
         return ExampleDAO.getExampleByName(name);
@@ -61,27 +41,10 @@ public final class ModelFacade implements IModel {
     /*
      * (non-Javadoc)
      * @see model.IModel#getAllExamples()
-     *
+     */
     @Override
     public List<Example> getAllExamples() throws SQLException {
         return ExampleDAO.getAllExamples();
     }
-
-    */
-    
-	@Override
-	public ResultSet getProcedureMotion(final String id){
-		return mapDao.getProcedureMotion(id);
-	}
-
-	@Override
-	public ResultSet getProcedureMotionLess(final String id){
-		return mapDao.getProcedureMotionLess(id);
-	}
-
-	@Override
-	public void CreateMap() {
-		
-	}
 
 }
--- model/src/main/java/model/immobile/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author Pc-Portable-LT
- *
- */
-package model.immobile;
